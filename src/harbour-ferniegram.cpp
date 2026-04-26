fc40fab4ea5f2441cf3127b0c77820322f0fe000
roundedrectangle <roundedrectangle0@gmail.com>
2026-04-26 20:16:55 +0300

    harbour-ferniegram.cpp: cosmetics

--- src/harbour-ferniegram.cpp
+++ src/harbour-ferniegram.cpp
@@ -1,25 +1,5 @@
-/*
-    Copyright (C) 2020 Sebastian J. Wolf and other contributors
-
-    This file is part of Fernschreiber.
-
-    Fernschreiber is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    Fernschreiber is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with Fernschreiber. If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #include <sailfishapp.h>
 #include <QQuickView>
-//#include <QtQml>
 #include <QQmlEngine>
 #include <QGuiApplication>
 #include <QSysInfo>
@@ -72,7 +52,7 @@ int main(int argc, char *argv[]) {
     context->setContextProperty("NO_HARBOUR_COMPLIANCE", false);
 #endif
 
-    view->setSource(SailfishApp::pathTo("qml/harbour-ferniegram.qml"));
+    view->setSource(SailfishApp::pathToMainQml());
     view->show();
     return app->exec();
 }
